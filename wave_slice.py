9ffa6b93f1fd429f1767dba8e42d52e9f20c96b6
SteveDeeds <70077400+SteveDeeds@users.noreply.github.com>
2024-12-09 17:21:20 -0800

    clean up comment

--- wave_slice.py
+++ wave_slice.py
@@ -27,7 +27,7 @@ def frequency_to_note(freq):
     
     A4 = 440.0
     notes = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
-    note_number = 12 * np.log2(freq / A4) + 69  # Corrected offset for A4 = MIDI 69
+    note_number = 12 * np.log2(freq / A4) + 69
     note_index = int(round(note_number)) % 12
     octave = (int(round(note_number)) // 12) - 1
     return f"{notes[note_index]}{octave}"
